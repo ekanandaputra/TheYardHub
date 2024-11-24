a72813dac5ed23c71ef01f9cf2d891d10f4d8fcd
ekananda <ekananda.putra12@gmail.com>
2024-11-24 08:12:05 +0700

    Update UI

--- app/src/main/java/com/ntech/theyardhub/feature/home/HomeScreen.kt
+++ app/src/main/java/com/ntech/theyardhub/feature/home/HomeScreen.kt
@@ -3,12 +3,23 @@ package com.ntech.theyardhub.feature.home
 import android.annotation.SuppressLint
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -16,9 +27,13 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -27,19 +42,17 @@ import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
 import com.ntech.theyardhub.R
-import com.ntech.theyardhub.core.ButtonHeight
-import com.ntech.theyardhub.core.ButtonType
-import com.ntech.theyardhub.core.component.GeneralButton
 import com.ntech.theyardhub.core.theme.Black
 import com.ntech.theyardhub.core.theme.Gray
 import com.ntech.theyardhub.core.theme.Typography
 import com.ntech.theyardhub.core.theme.White
 import com.ntech.theyardhub.core.theme.bluePrimary
 import com.ntech.theyardhub.core.utils.RoundedImageExample
-import com.ntech.theyardhub.core.utils.toRupiahFormat
+import com.ntech.theyardhub.datalayer.model.LocationModel
 import com.ntech.theyardhub.datalayer.model.PostModel
-import com.ntech.theyardhub.datalayer.model.ProductModel
+import com.ntech.theyardhub.datalayer.model.YardModel
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
@@ -53,8 +66,12 @@ fun HomeScreen(navController: NavController) {
         topBar = {
             TopAppBar(
                 title = {
-                    Row(verticalAlignment = Alignment.CenterVertically) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
                         val image: Painter = painterResource(id = R.drawable.icon)
+                        val sendIcon: Painter = painterResource(id = R.drawable.send)
                         Image(
                             painter = image,
                             contentDescription = "App Logo",
@@ -62,8 +79,15 @@ fun HomeScreen(navController: NavController) {
                         )
                         Text(
                             "YardHub",
-                            modifier = Modifier.padding(start = 8.dp),
+                            modifier = Modifier
+                                .padding(start = 8.dp)
+                                .weight(1f),
                             style = Typography.titleMedium.copy(fontWeight = FontWeight.ExtraBold)
+                        )
+                        Image(
+                            painter = sendIcon,
+                            contentDescription = "Send Icon",
+                            modifier = Modifier.size(24.dp)
                         )
                     }
                 },
@@ -147,13 +171,7 @@ fun ArticleHome() {
 
 @Composable
 fun ProductHome() {
-    val data: ArrayList<ProductModel> = arrayListOf(
-        ProductModel(name = "Product 1", price = 100000),
-        ProductModel(name = "Product 2", price = 250000),
-        ProductModel(name = "Product 3", price = 1250000),
-        ProductModel(name = "Product 4", price = 3000000),
-        ProductModel(name = "Product 5", price = 500000)
-    )
+    val data: List<YardModel> = generateYardModels()
 
     Column {
         Row(
@@ -161,7 +179,7 @@ fun ProductHome() {
             modifier = Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Text(text = "Recommended Product", style = Typography.titleMedium)
+            Text(text = "Farms", style = Typography.titleMedium)
             Text(text = "See all", style = Typography.labelSmall)
         }
         Spacer(modifier = Modifier.height(8.dp))
@@ -171,7 +189,7 @@ fun ProductHome() {
             contentPadding = PaddingValues(horizontal = 16.dp)
         ) {
             items(data.size) { index ->
-                ProductHomeItem(product = data[index], onClickItem = {})
+                YardHomeItem(item = data[index], onClickItem = {})
             }
         }
     }
@@ -214,38 +232,92 @@ fun ArticleHomeItem(post: PostModel = PostModel()) {
 }
 
 @Composable
-fun ProductHomeItem(product: ProductModel, onClickItem: (ProductModel) -> Unit) {
+fun YardHomeItem(item: YardModel, onClickItem: (YardModel) -> Unit) {
     Card(
         shape = RoundedCornerShape(10.dp),
         colors = CardDefaults.cardColors(White),
         modifier = Modifier
-            .clickable { onClickItem.invoke(product) }
+            .clickable { onClickItem.invoke(item) }
             .width(128.dp),
         elevation = CardDefaults.cardElevation(defaultElevation = 5.dp),
     ) {
         Column(
             modifier = Modifier
-                .padding(20.dp)
                 .fillMaxWidth()
         ) {
-            Image(
-                modifier = Modifier.fillMaxWidth(),
-                painter = painterResource(R.drawable.icon),
-                contentDescription = "avatar",
-                contentScale = ContentScale.FillWidth
+            AsyncImage(
+                model = "https://images-squarespace--cdn-com.translate.goog/content/v1/552ed2d1e4b0745abca6723d/3e60e68a-5ee9-4f49-9261-e890a6673173/grape+3.jpg?format=2500w&_x_tr_sl=en&_x_tr_tl=id&_x_tr_hl=id&_x_tr_pto=tc",
+                contentDescription = "Image from URL",
+                modifier = Modifier
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(color = Color.Black),
+                contentScale = ContentScale.Fit
             )
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(
-                text = product.name, style = Typography.titleSmall.copy(
-                    color = Black, fontWeight = FontWeight.Bold
+            Spacer(modifier = Modifier.height(8.dp))
+            Column(modifier = Modifier.padding(12.dp)) {
+                Text(
+                    text = item.name, style = Typography.titleSmall.copy(
+                        color = Black, fontWeight = FontWeight.Bold
+                    )
                 )
-            )
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(
-                text = product.price.toRupiahFormat(),
-                modifier = Modifier.padding(top = 5.dp),
-                style = Typography.bodyMedium
-            )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(
+                    text = item.locationModel.city,
+                    modifier = Modifier.padding(top = 5.dp),
+                    style = Typography.bodyMedium
+                )
+            }
         }
     }
+}
+
+fun generateYardModels(): List<YardModel> {
+    val yardModels = mutableListOf<YardModel>()
+
+    yardModels.add(
+        YardModel(
+            "Kebun Anggur Makmur",
+            "",
+            "Description for Yard 1",
+            LocationModel("Jakarta", -6.2088, 106.8456),
+            "uuid-1"
+        )
+    )
+    yardModels.add(
+        YardModel(
+            "Kebun Anggur Sejahtera",
+            "",
+            "Description for Yard 2",
+            LocationModel("Bandung", -6.9175, 107.6191),
+            "uuid-2"
+        )
+    )
+    yardModels.add(
+        YardModel(
+            "Kebun Anggur A",
+            "",
+            "Description for Yard 3",
+            LocationModel("Surabaya", -7.2504, 112.7688),
+            "uuid-3"
+        )
+    )
+    yardModels.add(
+        YardModel(
+            "Kebun Anggur B",
+            "",
+            "Description for Yard 4",
+            LocationModel("Yogyakarta", -7.7956, 110.3695),
+            "uuid-4"
+        )
+    )
+    yardModels.add(
+        YardModel(
+            "Kebun Anggur D",
+            "",
+            "Description for Yard 5",
+            LocationModel("Semarang", -6.9667, 110.4167),
+            "uuid-5"
+        )
+    )
+    return yardModels
 }
--- app/src/main/java/com/ntech/theyardhub/feature/splash/SplashScreen.kt
+++ app/src/main/java/com/ntech/theyardhub/feature/splash/SplashScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.ntech.theyardhub.R
 import com.ntech.theyardhub.core.RouteName.CHAT_LIST_SCREEN
+import com.ntech.theyardhub.core.RouteName.HOME_SCREEN
 import com.ntech.theyardhub.core.RouteName.SPLASH_SCREEN
 import com.ntech.theyardhub.core.theme.Typography
 import com.ntech.theyardhub.core.theme.White
@@ -35,7 +36,7 @@ fun SplashScreen(navController: NavController) {
 
     LaunchedEffect(key1 = true) {
         delay(3000L)
-        navController.navigate(CHAT_LIST_SCREEN) {
+        navController.navigate(HOME_SCREEN) {
             popUpTo(SPLASH_SCREEN) {
                 inclusive = true
             }
